36135c46bc4e27c15c87f02f875f8771889895f9
Yash <yashatkulwar2000@gmail.com>
2022-08-15 23:18:48 +0530

    add a new contact to address book

---
+++ Contact.java
@@ -0,0 +1,29 @@
+package Day9AddressBook;
+
+public class Contact {
+    String firstName, lastName, email;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
